31e8c78322dce9138410e229004b9abc8ad08c60
Nash <knshetty@live.com>
2015-09-04 04:03:03 +0300

    Ember: Setup environment & dependency for production on github-pages

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,13 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  vendorFiles: {
+    'handlebars.js': {
+      production: 'bower_components/handlebars/handlebars.js'
+    }
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
